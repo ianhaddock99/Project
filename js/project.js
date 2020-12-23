4f66c02ac3f4887ff2eaf08a1b853174dd735d74
Cedael White <84crose@gmail.com>
2020-12-23 11:16:37 -0500

    update

--- js/project.js
+++ js/project.js
@@ -1,33 +1,3 @@
-<<<<<<< HEAD
-// test
-
-
-
-<!doctype html>
-<html>
-<head>
-
-	<link rel="stylesheet" type="text/css" href="project.css">
-<title></title>
-</head>
-<body>
-	<header>
-		<h1>Groceries</h1>
-	</header>
-	<section id="basket">
-		<section id="basketItems"></section>
-		<form>
-            ITEM:
-  		    <input id="a" type="text" id name="firstname"></input>
-  			
-  			<button onclick="addList()">Button</button>
-		</form>
-	</section>
-	<footer></footer>
-	<script type="text/javascript" src="project.js"></script>
-</body>
-</html>
-=======
 
 let searchButton = document.querySelector("#search");
 
@@ -117,4 +87,3 @@ function renderModal(data) {
             `
 }
 
->>>>>>> main
