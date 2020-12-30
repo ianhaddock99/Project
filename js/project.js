ab014b5888029daa7231672246cfa2f49f555bb1
Ian Haddock <ianhaddock99@gmail.com>
2020-12-30 18:31:05 -0500

    verify

--- js/project.js
+++ js/project.js
@@ -1,8 +1,18 @@
 
 let searchButton = document.querySelector("#search");
+let searchButton1 = document.querySelector("#search1");
 
 searchButton.addEventListener("click", ()=>{
     let inputField = document.querySelector("#searchInput");
+        if (inputField.value.length <= 0) {
+            alert("Please insert a search term.")
+        } else {
+            sendApiRequest(inputField.value);
+        }
+})
+
+searchButton1.addEventListener("click", ()=>{
+    let inputField = document.querySelector("#searchInput1");
         if (inputField.value.length <= 0) {
             alert("Please insert a search term.")
         } else {
@@ -26,17 +36,28 @@ function useApiData(data){
     document.querySelector("#content").innerHTML = "";
     for (let i = 0;i < 5; i++) {
         let recipe = JSON.stringify(data.hits[i].recipe)
-        document.querySelector("#content").innerHTML += ` 
-            <img src="${data.hits[i].recipe.image}" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">${data.hits[i].recipe.label}</h5>
-                <p class="card-text">Health Labels: ${data.hits[i].recipe.healthLabels.join(", ")}</p>
-                <button onClick="renderModal(${JSON.stringify(recipe).split('"').join("&quot;")})" type="button" class="btn btn-primary" data-toggle="modal" data-target="#modal">Full Recipe</button>    
+        document.querySelector("#content").innerHTML += `
+        
+            <div class="card" style="width: 18rem;">
+                <img src="${data.hits[i].recipe.image}" class="card-img-top" alt="...">
+                <div class="card-body cp-bg-gold text-white d-flex flex-column justify-content-between">
+                <div>
+                    <h3 class="card-title">${data.hits[i].recipe.label}</h3>
+                    <p class="card-text">Health Labels: ${data.hits[i].recipe.healthLabels.join(", ")}</p>
+                </div>
+                    <button onClick="renderModal(${JSON.stringify(recipe).split('"').join("&quot;")})" type="button" class="btn btn-primary cp-bg-blue" data-toggle="modal" data-target="#modal">Full Recipe</button>
+                </div>
             </div>
+        
             `
     }
 }
 
+function generateIngList(ingList) {
+    return ingList
+            .map(element => `<li>${element}</li>`)
+            .join('');  
+}
 
 
 //////////GENERATES MODAL BOX WHEN MORE INFO BUTTON IS CLICKED ON THE CARD//////////////
@@ -45,19 +66,20 @@ function renderModal(data) {
     document.querySelector("#modal").innerHTML = `
         <div class="modal-dialog" role="document">
         <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">${parseData.label}</h5>
+            <div class="modal-header cp-bg-gold">
+                <h4 class="modal-title" id="exampleModalLabel">${parseData.label}</h4>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
                 </button>
             </div>
-            <div class="modal-body">
-                <li>
-                    ${parseData.ingredientLines}
-                </li>
+            <div class="modal-body cp-bg-blue cp-text-white">
+                <ul>
+                    ${generateIngList(parseData.ingredientLines)}                 
+                </ul>
             </div>
-            <div class="modal-body">
-                <hr>
+            <div class="modal-body cp-bg-blue cp-text-white">
+                <hr class="cp-bg-white">
+                <ul>
                 <li>
                     Calories: ${Math.round(parseData.calories/parseData.yield)}
                 </li>
@@ -76,10 +98,11 @@ function renderModal(data) {
                 <li>
                     Protein: ${Math.round(parseData.totalDaily.PROCNT.quantity/parseData.yield)}%
                 </li>
+                </ul>
             </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary" onclick="window.open('${parseData.url}');">Recipe Instructions</button>
+            <div class="modal-footer cp-bg-gold">
+                <button type="button" class="btn cp-bg-blue cp-text-white" data-dismiss="modal">Close</button>
+                <button type="button" class="btn cp-bg-blue cp-text-white" onclick="window.open('${parseData.url}');">Recipe Instructions</button>
             </div>
             </div>
             </div>
